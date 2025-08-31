13cb4537f3c5c078a98c0ac611dfae7c1fbe14a2
George <gogidgan95@gmail.com>
2025-08-31 07:29:04 +0300

    config.js variables names was changed

--- src/modules/core/config.js
+++ src/modules/core/config.js
@@ -2,18 +2,22 @@
 // Всё, что может меняться в зависимости от окружения
 // URLs, endpoints и т.д.
 
-export const API_ENDPOINTS = {
-    AUTH: 'https://api.directual.com/good/api/v5/auth?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b',
-    REG: 'https://api.directual.com/good/api/v5/data/registration/newUser?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
-    ADD_TEST_OBJ: 'https://api.directual.com/good/api/v5/data/testApi/newTestUser?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
-    EDIT_DEL_TEST_OBJ: 'https://api.directual.com/good/api/v5/data/inputtestapi/editTestObjects?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
-    ACTIVE_REMS: 'https://api.directual.com/good/api/v5/data/reminds/activeReminders?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
-    COMPLETE_REMS: 'https://api.directual.com/good/api/v5/data/reminds/completedReminders?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
-    GRAFT_REM: 'https://api.directual.com/good/api/v5/data/inputremind/newDraftRemind?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
-    CONFIRM_REM: 'https://api.directual.com/good/api/v5/data/inputremind/confirmRemind?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
-};
+export const CONFIG = {
 
-export const LOCAL_STORAGE_ITEMS = {
-    SESSION_ID: localStorage.getItem('sessionID'),
-    ROLE: localStorage.getItem('role'),
-}
+    API_ENDPOINTS: {
+        AUTH: 'https://api.directual.com/good/api/v5/auth?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b',
+        REG: 'https://api.directual.com/good/api/v5/data/registration/newUser?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
+        ADD_TEST_OBJ: 'https://api.directual.com/good/api/v5/data/testApi/newTestUser?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
+        EDIT_DEL_TEST_OBJ: 'https://api.directual.com/good/api/v5/data/inputtestapi/editTestObjects?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
+        ACTIVE_REMS: 'https://api.directual.com/good/api/v5/data/reminds/activeReminders?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
+        COMPLETE_REMS: 'https://api.directual.com/good/api/v5/data/reminds/completedReminders?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
+        GRAFT_REM: 'https://api.directual.com/good/api/v5/data/inputremind/newDraftRemind?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
+        CONFIRM_REM: 'https://api.directual.com/good/api/v5/data/inputremind/confirmRemind?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=',
+    },
+
+    LOCAL_STORAGE: {
+        SESSION_ID: localStorage.getItem('sessionID'),
+        ROLE: localStorage.getItem('role'),
+    },
+
+};
